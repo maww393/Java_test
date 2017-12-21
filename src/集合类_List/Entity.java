699739ddafb139194b0a3d4e09511e554feff16f
maww393 <2629055992@qq.com>
2017-12-21 21:40:40 +0800

    1

---
+++ src/集合类_List/Entity.java
@@ -0,0 +1,35 @@
+package 集合类_List;
+
+public class Entity {
+
+     private int id;
+     private int ParentId;
+     private String username;
+     private String content;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getParentId() {
+		return ParentId;
+	}
+	public void setParentId(int parentId) {
+		ParentId = parentId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+     
+     
+}
